e81596a5091aaac6cacaf12feb649cf2c66e3e89
huythai855 <nguyenhuythai15@gmail.com>
2023-04-19 18:37:28 +0700

    update Web Server

---
+++ WebServer/Web_server.py
@@ -0,0 +1,44 @@
+# import socket module
+from socket import *
+import sys  # In order to terminate the program
+
+serverPort = 6789
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# Prepare a sever socket
+# Fill in start
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+# Fill in end
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()  # Fill in start #Fill in end
+    try:
+        message = connectionSocket.recv(1024)
+        # Fill in start #Fill in end
+        filename = message.split()[1]
+        f = open(filename[1:])
+        output_data = f.read()  # Fill in start
+        print(output_data)
+        # #Fill in end
+        # Send one HTTP header line into socket
+        # Fill in start
+        connectionSocket.send('\nHTTP/1.1 200 OK\n\n'.encode())
+        # Fill in end
+        # Send the content of the requested file to the client
+        for i in range(0, len(output_data)):
+            connectionSocket.send(output_data[i].encode())
+        connectionSocket.send("\r\n".encode())
+
+        connectionSocket.close()
+    except IOError:
+        # Send response message for file not found
+        # Fill in start
+        connectionSocket.send("\nHTTP/1.1 404 Not Found\n\n".encode())
+        # Fill in end
+        # Close client socket
+        # Fill in start
+        # Fill in end
+serverSocket.close()
+sys.exit()  # Terminate the program after sending the corresponding data
